b422bb40954763fada84b529f5b53b23694ad2ac
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-05 18:19:05 -0500

    setup auth routes and headerwrapper component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,19 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 import "./style/main.scss";
+import SignUp from "./components/auth/signUp";
+import SignIn from "./components/auth/signIn";
+import HeaderWrapper from "./components/headerWrapper";
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+          <HeaderWrapper>
+            <Route path="/" exact component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+          </HeaderWrapper>
+        </Switch>
       </BrowserRouter>
     </Provider>,
     document.querySelector(".app-wrapper")
---
+++ src/components/auth/signUp.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  render() {
+    return <div className="sign-up">...SignUp</div>;
+  }
+}
+
+export default SignUp;
