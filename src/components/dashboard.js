dec3cd06585279ed258b0f907746c8d15098ea2c
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-07 18:35:30 -0500

    added callback to tabnav to handle clicking a tab

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -9,7 +9,7 @@ class Dashboard extends Component {
       tabs: [
         {
           title: "Newsletter",
-          active: false,
+          active: true,
           component: <h4>Hey There - Newsletter</h4>,
         },
         {
@@ -21,10 +21,15 @@ class Dashboard extends Component {
     };
   }
 
+  handleTabChange = (title) => {};
+
   render() {
     return (
       <div className="dashboard">
-        <TabNav tabs={this.state.tabs} />
+        <TabNav
+          handleClick={(title) => this.handleTabChange(title)}
+          tabs={this.state.tabs}
+        />
       </div>
     );
   }
