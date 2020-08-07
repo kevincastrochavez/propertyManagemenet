4596b6e4cce99ca4f933b9c512f7ebf5083676f7
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-06 20:11:46 -0500

    AUTHENTICATE USER reducer

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { ROOT_URL } from "../config";
+import { AUTHENTICATE_USER } from "./types";
 
 export function signUp(fields, success) {
   return function (dispatch) {
     axios
       .post(`${ROOT_URL}/signUp`, fields)
       .then((response) => {
-        console.log(response);
+        dispatch({
+          type: AUTHENTICATE_USER,
+          payload: response.data,
+        });
         success();
       })
       .catch((err) => {
