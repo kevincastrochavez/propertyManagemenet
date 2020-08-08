a14c630cb1d4a6408eec948eab5f1db8f562a4e6
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-08 13:45:28 -0500

    mapped over a newsletter JSON item to the latest newsletter component

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -5,11 +5,20 @@ import NewsletterLatest from "./newsletterLatest";
 
 class NewsletterGrid extends Component {
   render() {
+    const latest = {
+      _id: "51861841",
+      title: "Happy Holidays",
+      body:
+        "is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Loremis simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem",
+      date: new Date(),
+      imageUrl: "http://via.placeholder.com/700x258",
+    };
+
     return (
       <div className="newsletter-grid">
         <NewsletterBox date={new Date()} />
         <NewsletterArchive />
-        <NewsletterLatest />
+        <NewsletterLatest {...latest} />
       </div>
     );
   }
--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,28 +2,17 @@ import React, { Component } from "react";
 
 class NewsletterLatest extends Component {
   render() {
+    const { title, imageUrl, body } = this.props;
     return (
       <div className="newsletter-latest">
-        <h1 className="newsletter-latest__title">Title</h1>
-        <img
-          className="newsletter-latest__image"
-          src="http://via.placeholder.com/700x258"
-        ></img>
+        <h1 className="newsletter-latest__title">{title}</h1>
+        <img className="newsletter-latest__image" src={imageUrl}></img>
         <div className="newsletter-latest__body">
-          <p>
-            is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s, when an unknown printer took a galley of type and scrambled
-            it to make a type specimen book. It has survived not only five
-            centuries, but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more
-            recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum
-          </p>
+          <p>{body}</p>
         </div>
       </div>
     );
   }
 }
+
 export default NewsletterLatest;
