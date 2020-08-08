7fb1232e408647e1e82857e0206757e929a9ada7
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-07 20:19:17 -0500

    built archive and archive item components and placed on grid

---
+++ src/components/newsletter/newsletterArchive.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+function ArchiveItem({ title, date }) {
+  return (
+    <div className="archive-item">
+      <div className="archive-item__title">{title}</div>
+
+      <div className="archive-item__date">{date}</div>
+    </div>
+  );
+}
+
+class NewsletterArchive extends Component {
+  render() {
+    return (
+      <div className="newsletter-archive">
+        <div className="newsletter-archive__title">Archive</div>
+
+        <div className="newsletter-archive__items">
+          <ArchiveItem title="Hey" date="Today" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsletterArchive;
