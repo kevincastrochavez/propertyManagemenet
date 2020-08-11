640d1fecc5b52b84a5abb5e3b8d637c38f863395
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-11 13:13:51 -0500

    started building requests feature

---
+++ src/components/requests/requestsGrid.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RequestsGrid extends Component {
+  render() {
+    return <div className="requests-grid">Requests Grid</div>;
+  }
+}
+
+export default RequestsGrid;
