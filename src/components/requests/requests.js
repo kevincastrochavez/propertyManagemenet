fd025d6406e5b9a565ec6083512c98e855aaf606
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-11 19:28:57 -0500

    built requests item component base and applied a grid to requests component

--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
+import RequestsItem from "./requestsItem";
 
 class Requests extends Component {
   render() {
     return (
       <div className="requests">
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-        <div>Item 4</div>
+        <RequestsItem />
       </div>
     );
   }
---
+++ src/components/requests/requestsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RequestsItem extends Component {
+  render() {
+    return <div className="requests-item">Item goes here</div>;
+  }
+}
+
+export default RequestsItem;
