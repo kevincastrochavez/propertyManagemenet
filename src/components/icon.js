cc6cbc6999610e950b348fbcfe6a8a7af4697a0e
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-11 19:47:19 -0500

    JSX for request item

--- src/components/icon.js
+++ src/components/icon.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function Icon({ icon }) {
-  return <i className={icon}></i>;
+export default function Icon({ className, icon }) {
+  return (
+    <div className={className}>
+      <i className={icon} />
+    </div>
+  );
 }
--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import Icon from "../icon";
+import Button from "../button";
 
 class RequestsItem extends Component {
   render() {
     return (
       <div className="requests-item">
-        <div>
-          <Icon icon="fas fa-exclamation-triangle" />
-        </div>
+        <Icon
+          className="requests-item__icon"
+          icon="fas fa-exclamation-triangle"
+        />
+
+        <div className="requests-item__title">Broken bathroom</div>
+
+        <div className="requests-item__tenant-unit">Kevin - Unit 204</div>
+
+        <Icon className="requests-item__arrow" icon="fas fa-sort-down" />
+
+        <div className="requests-item__date">23/03/1997</div>
+
+        <Button
+          className="requests-item__move"
+          icon="fas fa-wrench"
+          callback={() => console.log("try to move")}
+        />
       </div>
     );
   }
