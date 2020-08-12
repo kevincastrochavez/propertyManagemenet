31398631c01009bf3f1ea7990489aa105239efaf
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-12 13:06:12 -0500

    build create new request action creator with redux-thunk middleware

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -1,8 +1,24 @@
 import { CHANGE_SELECTED_REQUEST_TYPE } from "./types";
+import { ROOT_URL } from "../config";
+import axios from "axios";
 
 export function changeSelectedRequestType(boxType) {
   return {
     type: CHANGE_SELECTED_REQUEST_TYPE,
     payload: boxType,
+  };
+}
+
+export function createNewRequest(newRequest, success) {
+  return function () {
+    axios
+      .post(`${ROOT_URL}/requests/new`, newRequest)
+      .then((response) => {
+        console.log(response.data);
+        success();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 }
