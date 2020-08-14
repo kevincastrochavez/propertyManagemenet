6135d27a4650c402a3e97b5263894c5556c9bf40
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-08-13 19:31:51 -0500

    refactored fetchnewsletters to actually fetch from the backend

--- src/actions/newsletter.js
+++ src/actions/newsletter.js
@@ -1,30 +1,23 @@
 import { SET_NEWSLETTERS, FETCH_NEWSLETTER_ID } from "./types";
+import axios from "axios";
+import { ROOT_URL } from "../config";
 
 export function fetchNewsletters() {
-  const response = {
-    data: [
-      {
-        _id: "51861841",
-        title: "Happy Holidays",
-        body:
-          "is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Loremis simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem",
-        date: new Date(),
-        imageUrl: "http://via.placeholder.com/700x258",
-      },
-      {
-        _id: "68766484",
-        title: "Happy Christmas",
-        body:
-          "Heeeeeeeeeeeeeeeeeeeeeeeeey text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem is simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Loremis simply dummy text of the printing and typesetting industry. Loris simply dummy text of the printing and typesetting industry. Lorem",
-        date: new Date(),
-        imageUrl: "http://via.placeholder.com/700x258",
-      },
-    ],
-  };
+  return function (dispatch) {
+    const token = localStorage.getItem("token");
+    const headers = { headers: { authorization: token } };
 
-  return {
-    type: SET_NEWSLETTERS,
-    payload: response.data,
+    axios
+      .get(`${ROOT_URL}/newsletters`, headers)
+      .then((response) => {
+        dispatch({
+          type: SET_NEWSLETTERS,
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 }
 
